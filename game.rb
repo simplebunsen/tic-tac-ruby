d09f9b7445f72281a2274b3bd247ca760d4d7833
simplebunsen <52760596+simplebunsen@users.noreply.github.com>
2021-05-25 21:41:22 +0200

    progress in method structure for board

--- game.rb
+++ game.rb
@@ -1,18 +1,28 @@
 class Board
   @@TYPE_X = "[X]"
   @@TYPE_O = "[O]"
-  @@TYPE_EMPTY = "[_]"
-  
+  @@TYPE_EMPTY = "[_]"  
 
-  def self.empty()
-    board = []
-
+  def initialize()
     3.times do 
+      @board = []
       3.times {board.push Field.new(@@TYPE_EMPTY)}
     end
-
-    return board
   end
+
+  def beautify()
+    #returns beautiful rendition of the current board
+  end
+
+  def move(x, y, type)
+    #inputs move of type Z at X,Y
+  end
+
+  def winner?()
+    #checks if board has winner (or draw) and returns the winner type
+  end
+
+
 end
 
 
@@ -29,5 +39,3 @@ class Field < Board
   end
 
 end
-
-puts Field.empty_board
