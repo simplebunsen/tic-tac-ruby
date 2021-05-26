3b0bfcdfd2e4547aee81a6ddffcde8a74e35d9f2
simplebunsen <52760596+simplebunsen@users.noreply.github.com>
2021-05-26 20:27:19 +0200

    basic board printing, refractored Field states to their own module

--- game.rb
+++ game.rb
@@ -1,16 +1,20 @@
-# A playing board
-class Board
+# module containing Field States
+module FieldHelper
   TYPE_X = '[X]'.freeze
   TYPE_O = '[O]'.freeze
   TYPE_EMPTY = '[_]'.freeze
+end
 
+# A playing board
+class Board
+  include FieldHelper
   def initialize
     @board = Array.new(3) { Array.new(3, TYPE_EMPTY) }
   end
 
   def beautify()
     # returns beautiful rendition of the current board
-
+    puts @board
   end
 
   def move(x, y, type)
@@ -26,6 +30,7 @@ end
 
 # A single field on a board
 class Field < Board
+  include FieldHelper
   attr_accessor :type
 
   def initialize(type)
