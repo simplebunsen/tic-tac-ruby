97fa78a37ba99ff6fb9c546fe7adad823259025c
simplebunsen <52760596+simplebunsen@users.noreply.github.com>
2021-05-26 20:24:56 +0200

    better way to init array

--- game.rb
+++ game.rb
@@ -1,32 +1,30 @@
+# A playing board
 class Board
-  @@TYPE_X = "[X]"
-  @@TYPE_O = "[O]"
-  @@TYPE_EMPTY = "[_]"  
+  TYPE_X = '[X]'.freeze
+  TYPE_O = '[O]'.freeze
+  TYPE_EMPTY = '[_]'.freeze
 
-  def initialize()
-    3.times do 
-      @board = []
-      3.times {board.push Field.new(@@TYPE_EMPTY)}
-    end
+  def initialize
+    @board = Array.new(3) { Array.new(3, TYPE_EMPTY) }
   end
 
   def beautify()
-    #returns beautiful rendition of the current board
+    # returns beautiful rendition of the current board
+
   end
 
   def move(x, y, type)
-    #inputs move of type Z at X,Y
+    # inputs move of type Z at X,Y (if not illegal!)
   end
 
   def winner?()
-    #checks if board has winner (or draw) and returns the winner type
+    # checks if board has winner (or draw) and returns the winner type
   end
 
 
 end
 
-
-
+# A single field on a board
 class Field < Board
   attr_accessor :type
 
@@ -34,7 +32,7 @@ class Field < Board
     @type = type
   end
 
-  def to_s()
+  def to_s
     return @type
   end
 
